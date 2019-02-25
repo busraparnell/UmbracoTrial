0cd23af786c1baa98379e0aaf8be646bfb8a1fcb
busraparnell <aysebusrasengul@gmail.com>
2019-02-25 12:15:31 +0000

    Customise Section element added

---
+++ App_Plugins/Example/Application/PersonSection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.interfaces;
+using umbraco.businesslogic;
+
+namespace WebApp.App_Plugins.Example.Application
+{
+    [Application("example","Example","icon-people",15)]
+    public class PersonSection : IApplication
+
+    {
+    }
+}
