7786395e1176e28631c8e6abb9a3a65dc0678c2b
busraparnell <aysebusrasengul@gmail.com>
2019-02-25 14:44:46 +0000

    Methods to use with Database

---
+++ App_Plugins/Example/Objects/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace WebApp.App_Plugins.Example.Application.Objects
+{
+    [TableName("Person")]
+    public class Person
+    {
+
+        public Person()
+        {
+
+        }
+        [PrimaryKeyColumn(AutoIncrement = true)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ App_Plugins/Example/personResource.js
@@ -0,0 +1,14 @@
+﻿angular.module("umbraco.resource")
+    .factory("personResource", function ($http) {
+        return {
+            GetById; function(id) {
+                return $http.get("backoffice/Example/PersonApi/GetById?id=" + id)
+            },
+            save: function (person) {
+                return $http.post("backoffice/Example/PersonApi/PostSave", angular.toJson(person));
+            },
+            deleteById: function (id) {
+                return $http.delete("backoffice/Example/PersonApi/DeleteById=" + id)
+            }
+        }
+    });
